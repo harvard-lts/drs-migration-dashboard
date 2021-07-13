8cdc8ccf9006a959073f8a0206ca511ee49c2d70
Mr Phil Plencner <philip_plencner@harvard.edu>
2021-07-13 15:45:58 -0400

    Adding route prefix. Adding docker stuff for dev deployment.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 import os, click
 from flask import Flask, current_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.wrappers import Response
 
 # Import custom modules from the local project
 # Import API resources
@@ -9,6 +11,11 @@ from . import resources
 def create_app():
   # Create and configure the app
   app = Flask(__name__, instance_relative_config=True)
+  app.wsgi_app = DispatcherMiddleware(
+      Response('Not Found', status=404),
+      {'/migrationstatus': app.wsgi_app}
+  )
+
   # App config
   app.config.from_mapping(
       ROOT_ROUTE = '/'
