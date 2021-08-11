0444f42c6dfeb6fdde567fb41b7ef60720a3088b
Mr Phil Plencner <philip_plencner@harvard.edu>
2021-08-11 11:42:58 -0400

    Adding redirect from piechart to homepage.

--- app/resources.py
+++ app/resources.py
@@ -1,5 +1,5 @@
 from flask_restx import Resource
-from flask import request, current_app, make_response, jsonify, render_template
+from flask import request, current_app, make_response, jsonify, render_template, redirect, url_for
 import os, json
 
 # Import proxy file
@@ -7,5 +7,8 @@ import os, json
 
 def define_resources(app):
     @app.route('/')
-    def piechart():
+    def status():
         return render_template('status.html')
+    @app.route('/piechart/')
+    def piechart():
+        return redirect(url_for('status'))
